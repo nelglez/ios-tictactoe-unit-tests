1e4b7080d8caabc035c1c53791e6ee39de07631e
Nelson Gonzalez <nelglezfl@gmail.com>
2019-02-27 15:50:12 -0500

    testCatsGame

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -133,7 +133,7 @@ class GameAITests: XCTestCase {
         */
         
         try! board.place(mark: .x, on: (0,0))
-        try! board.place(mark: .x, on: (0,2))
+        try! board.place(mark: .x, on: (2,0))
         try! board.place(mark: .x, on: (1,1))
         try! board.place(mark: .x, on: (1,2))
         try! board.place(mark: .o, on: (1,0))
@@ -146,5 +146,23 @@ class GameAITests: XCTestCase {
     }
 
     func testCatsGame() {
+        /*
+         x o x
+         o x o
+         o x o
+         */
+        
+        try! board.place(mark: .x, on: (0,0))
+        try! board.place(mark: .x, on: (2,0))
+        try! board.place(mark: .x, on: (1,1))
+        try! board.place(mark: .x, on: (1,2))
+        try! board.place(mark: .o, on: (1,0))
+        try! board.place(mark: .o, on: (0,1))
+        try! board.place(mark: .o, on: (2,1))
+        try! board.place(mark: .o, on: (2,2))
+        try! board.place(mark: .o, on: (0,2))
+        XCTAssertTrue(board.isFull)
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
     }
 }
